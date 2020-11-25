d6b5b5746dde59df82d07c843ba2a4b8e1c032f1
Carolxne <caroline.wen3@gmail.com>
2020-11-25 13:06:57 +0000

    hamburger to exit sign, and centred links

---
+++ script.js
@@ -0,0 +1,11 @@
+//hamburger menu links//
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-menu');
+
+// Mobile menu display
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
