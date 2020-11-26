a79e52228dc3ef589b00871106010847f4515039
Carolxne <caroline.wen3@gmail.com>
2020-11-26 05:42:54 +0000

    Hero Section

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//hamburger menu links//
+/*hamburger menu links*/
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
 
-// Mobile menu display
+/* Mobile menu display*/
 const mobileMenu = () => {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
